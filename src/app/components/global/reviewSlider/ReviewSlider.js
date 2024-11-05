b82a84e228bef5560b7207bf0a41ed647ffd9d4f
Satish <satishpati96548@gmail.com>
2024-11-05 13:04:49 +0530

    global var and responsive

--- src/app/components/global/reviewSlider/ReviewSlider.js
+++ src/app/components/global/reviewSlider/ReviewSlider.js
@@ -15,18 +15,21 @@ const ReviewSlider = () => {
         {/* Bottom Review Section */}
         <div className={styles.reviewBotSection}>
           <div className={styles.firstBotReview}>
-            <h3 className={styles.mainHeading}>Courses Top Rated in:</h3>
-            <p>Discover what our learners say about us</p>
-            <Image
-              src="https://d32and0ii3b8oy.cloudfront.net/web/V4/course_iit_guwahati/Social_review.webp"
-              alt="Learnbay"
-              quality={100}
-              priority
-              width="800"
-              height="408"
-              className={styles.socailR}
+            <div className={styles.firstBotHeading}>
+              <h3 className={styles.mainHeading}>Courses Top Rated in:</h3>
+              <p>Discover what our learners say about us</p>
+            </div>
+            <div className={styles.firstBotImgWrapper}>
+              <Image
+                src="https://d32and0ii3b8oy.cloudfront.net/web/V4/course_iit_guwahati/Social_review.webp"
+                alt="Learnbay"
+                quality={100}
+                priority
+                fill
+              // className={styles.socailR}
 
-            />
+              />
+            </div>
           </div>
           <div className={styles.secondBotReview}>
             <VideoYt className={styles.YT} />
