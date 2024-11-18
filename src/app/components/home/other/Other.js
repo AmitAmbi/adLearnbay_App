ab4933eb1e2364a9ba940d0ed55bb58f07f634ca
Satish <satishpati96548@gmail.com>
2024-11-18 16:27:50 +0530

    other completed

--- src/app/components/home/other/Other.js
+++ src/app/components/home/other/Other.js
@@ -52,84 +52,86 @@ const Other = () => {
   };
 
   return (
-    <div className={styles.mainContainer}>
-      <h2 className={styles.h2}>{otherData.title}</h2>
+    <div className="containerWidth">
+      <div className={styles.mainContainer}>
+        <h2 className={styles.h2}>{otherData.title}</h2>
 
-      <div className={styles.HeadDiv} ref={headDivRef}>
-        {/* Left Side */}
-        <div className={styles.left}>
-          {otherData.leftSide.map((item, index) => (
-            <div className={styles.inside} key={index}>
-              <p>{item.label}</p>
-              {otherData.svgArrow(item.svgWidth, item.svgHeight)}
-            </div>
-          ))}
-        </div>
-
-        {/* Middle Side */}
-        <div className={styles.middle}>
-          <div className={styles.imgdiv}>
-            <Image
-              src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/learnbay_logo_others.webp"
-              width={200}
-              height={40}
-              alt="learnbay"
-              loading="lazy"
-            />
+        <div className={styles.HeadDiv} ref={headDivRef}>
+          {/* Left Side */}
+          <div className={styles.left}>
+            {otherData.leftSide.map((item, index) => (
+              <div className={styles.inside} key={index}>
+                <p>{item.label}</p>
+                {otherData.svgArrow(item.svgWidth, item.svgHeight)}
+              </div>
+            ))}
           </div>
 
-          {otherData.middleSide.map((content, index) => (
-            <div key={index}>
-              <div className={styles.middleContent}>
-                <Image
-                  src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/sign_mark.webp"
-                  width={25}
-                  height={25}
-                  alt="icon"
-                  priority
-                  quality={80}
-                />
-                <p>{content}</p>
-              </div>
-              {index < otherData.middleSide.length - 1 && <hr />}
+          {/* Middle Side */}
+          <div className={styles.middle}>
+            <div className={styles.imgdiv}>
+              <Image
+                src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/learnbay_logo_others.webp"
+                width={200}
+                height={40}
+                alt="learnbay"
+                loading="lazy"
+              />
             </div>
-          ))}
+
+            {otherData.middleSide.map((content, index) => (
+              <div key={index}>
+                <div className={styles.middleContent}>
+                  <Image
+                    src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/sign_mark.webp"
+                    width={25}
+                    height={25}
+                    alt="icon"
+                    priority
+                    quality={80}
+                  />
+                  <p>{content}</p>
+                </div>
+                {index < otherData.middleSide.length - 1 && <hr />}
+              </div>
+            ))}
+          </div>
+
+          {/* Right Side */}
+          <div className={styles.right}>
+            <h4>Others</h4>
+            {otherData.rightSide.map((content, index) => (
+              <div key={index}>
+                <div className={styles.rytInside}>
+                  <Image
+                    src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/wrong_icon.webp"
+                    width={30}
+                    height={30}
+                    alt="icon"
+                    priority
+                    quality={80}
+                  />
+                  <p>{content}</p>
+                </div>
+                {index < otherData.rightSide.length - 1 && <hr />}
+              </div>
+            ))}
+          </div>
         </div>
 
-        {/* Right Side */}
-        <div className={styles.right}>
-          <h4>Others</h4>
-          {otherData.rightSide.map((content, index) => (
-            <div key={index}>
-              <div className={styles.rytInside}>
-                <Image
-                  src="https://d32and0ii3b8oy.cloudfront.net/web/s3_main/Course-home/wrong_icon.webp"
-                  width={30}
-                  height={30}
-                  alt="icon"
-                  priority
-                  quality={80}
-                />
-                <p>{content}</p>
-              </div>
-              {index < otherData.rightSide.length - 1 && <hr />}
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Custom Scrollbar */}
-      <div className={styles.customScrollbar}>
-        <div className={styles.dotContainer}>
-          {/* Clicking these dots will scroll to the corresponding section */}
-          <span
-            onClick={() => scrollToSection(0)}
-            className={`${styles.dotOne}  ${activeDot === 0 ? styles.active : ""}`}
-          />
-          <span
-            onClick={() => scrollToSection(1)}
-            className={`${styles.dot} ${activeDot === 1 ? styles.active : ""}`}
-          />
+        {/* Custom Scrollbar */}
+        <div className={styles.customScrollbar}>
+          <div className={styles.dotContainer}>
+            {/* Clicking these dots will scroll to the corresponding section */}
+            <span
+              onClick={() => scrollToSection(0)}
+              className={`${styles.dotOne}  ${activeDot === 0 ? styles.active : ""}`}
+            />
+            <span
+              onClick={() => scrollToSection(1)}
+              className={`${styles.dot} ${activeDot === 1 ? styles.active : ""}`}
+            />
+          </div>
         </div>
       </div>
     </div>
